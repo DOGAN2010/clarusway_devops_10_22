93d584f44a803a446dd377572d699f9f70a76ca4
DOGAN2010 <doganbilisim2010@gmail.com>
2022-07-21 13:21:30 +0300

    new commit

--- hands-on/Docker/docker-01-installing-on-ec2-linux2/ec2-docker.tf
+++ hands-on/Docker/docker-01-installing-on-ec2-linux2/ec2-docker.tf
@@ -46,7 +46,7 @@ resource "aws_security_group" "allow_ssh" {
 }
 
 resource "aws_instance" "tf-ec2" {
-  ami           = "ami-087c17d1fe0178315"
+  ami           = "ami-0cff7528ff583bf9a"
   instance_type = var.instance-type
   key_name = "tyler-team"
   vpc_security_group_ids = [ aws_security_group.allow_ssh.id ]
